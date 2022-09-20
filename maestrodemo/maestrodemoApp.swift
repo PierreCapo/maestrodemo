cb8f45b70a688dcb0b6d19d7364aad3f754e8046
PierreCapo <pierre.caporossi@gadz.org>
2022-09-20 12:10:16 +0200

    Initial Commit

---
+++ maestrodemo/maestrodemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  maestrodemoApp.swift
+//  maestrodemo
+//
+//  Created by Pierre Caporossi on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct maestrodemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
